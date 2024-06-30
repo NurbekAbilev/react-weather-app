1be4fc51a47e7b8fb2cc03cee2490084a3be4173
Nurbek Abilev <abilev.nurbek@gmail.com>
2024-07-01 02:30:57 +0500

    commit

---
+++ src/components/Search.js
@@ -0,0 +1,75 @@
+import { useCallback, useEffect, useState } from "react";
+
+const API_KEY = '998ec8c1dd074e61965223423242806'
+
+const debounce = (func, delay) => {
+    let timer;
+    return (...args) => {
+        if (timer) {
+            clearTimeout(timer);
+        }
+        timer = setTimeout(() => {
+            func(...args);
+        }, delay);
+    };
+};
+
+async function querySearch(cityName) {
+    const response = await fetch(`http://api.weatherapi.com/v1/search.json?key=${API_KEY}&q=${cityName}`)
+    const jsonResponse = await response.json()
+    console.log('city name', cityName)
+    console.log('json response', jsonResponse)
+    return jsonResponse
+}
+
+export default function Search() {
+    const [inputValue, setInputValue] = useState('')
+    let [autoCompleteElements, setAutoCompleteElements] = useState([])
+
+    function add() {
+        setAutoCompleteElements([...autoCompleteElements, 'New York'])
+    }
+
+    function remove() {
+        const newAutoCompleteElements = autoCompleteElements.slice()
+        newAutoCompleteElements.pop()
+        setAutoCompleteElements(newAutoCompleteElements)
+    }
+
+    const debouncedSetCities = useCallback(debounce(() => {
+        const data = querySearch(inputValue).then((response) => {
+            console.log('response', data)
+        })
+    }, 1500), [inputValue])
+
+    function handleChange(event) {
+        setInputValue(event.target.value)
+    }
+
+    // Update debounced value when inputValue changes
+    useEffect(() => {
+        if (inputValue.trim()) {
+            debouncedSetCities(inputValue);
+        } else {
+            setAutoCompleteElements([]);
+        }
+    }, [inputValue]);
+
+    return (
+        <>
+            <button onClick={add}>Add</button>
+            <button onClick={remove}>Remove</button>
+            <h2>Weather app</h2>
+            <div className="app-search">
+                <input onChange={handleChange} className="app-search-input" autoComplete="off" placeholder="Enter your city name"></input>
+                <button className="app-search-button"><img className="app-search-img" alt="search" src="/images/search.png" /></button>
+            </div>
+            <div className="app-search-autocomplete-container">
+                {autoCompleteElements.map((el) =>
+                    <div className="app-search-autocomplete-element">{el}</div>
+                )}
+            </div>
+        </>
+
+    );
+}
